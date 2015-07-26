6f3f9e75cb6c35170191f6c984dd2d37ade5ec22
FaustXVI <xavier.detant@gmail.com>
2015-07-26 12:45:41 +0200

    Initialize project with Spring initializer
    Use gradle as the build tool.
    Allow actuators.
    Prepare for remote shell and HATEOAS RESTful webservice.

---
+++ src/main/java/org/qball/world/QBallWorldApplication.java
@@ -0,0 +1,12 @@
+package org.qball.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBallWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QBallWorldApplication.class, args);
+    }
+}
